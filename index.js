d2996ecc0bf7024fb5a75192232f07fd709c99d0
zainyamcgarrell <zainyamcgarrell@penguin>
2021-01-23 21:31:21 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var itemPrice = 4;
+var amountOfmoneyInwallet = 20;
+var numberOffriends = 9;
+var ageInyears = 0.25;
+var firstName = 'Zainya'
+var lastName = 'Mcgarrell'
+var middleInitial = 'l'
+
+var y = 4  
+var x = 20
+
+console.log(x - y);
+
+console.log(firstName + middleInitial + lastName);
+
+console.log(ageInyears/numberOffriends);
+
+console.log(itemPrice+numberOffriends-ageInyears);
